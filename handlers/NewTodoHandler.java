d97e6ceb1ca2d894747ba3e021b326db00f52d6a
Monika Lobinger <monika.lobinger@gmx.de>
2014-09-28 18:36:17 +0200

    6 Neue Dateien committed

---
+++ handlers/NewTodoHandler.java
@@ -0,0 +1,27 @@
+package my.test.notes.handlers;
+
+import java.util.Date;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import my.test.notes.model.ITodoService;
+import my.test.notes.model.Todo;
+import my.test.notes.wizards.TodoWizard;
+
+public class NewTodoHandler
+{
+@Execute
+public void execute(Shell shell, ITodoService todoService)
+{
+    Todo todo = new Todo();
+    todo.setDueDate(new Date());
+    WizardDialog dialog = new WizardDialog(shell, new TodoWizard(todo));
+    if(dialog.open() == WizardDialog.OK) {
+        // call service to save Todo object
+        todoService.saveTodo(todo);
+    }
+
+}
+}
